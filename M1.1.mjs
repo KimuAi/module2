87949c1a16f0a06b2d8f1c41870024da35665a99
Kimberley Thill <ThillKimberleyPro@gmail.com>
2024-09-30 10:00:51 +0200

    M1.1

---
+++ M1.1.mjs
@@ -0,0 +1,32 @@
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+
+let keuze = parseFloat(await userInput.question ("Geef je keuze in: \n1.Red Hot Chilli Peppers\n2.AC/DC\n3.Netsky\n4.De Strangers\nGeef je keuze in: "));
+
+switch(keuze)
+{
+    case 1:
+        console.log("Je hebt gekozen voor " + '"' + "Red Hot Chilli Peppers" + '"')
+        console.log("Ze zijn de artist achter de hit: Otherside ")
+    break;
+
+    case 2:
+        console.log("Je hebt gekozen voor " + '"' + "AC/DC" + '"')
+        console.log("Ze zijn de artist achter de hit: Thunderstruck")
+    break;
+
+    case 3:
+        console.log("Je hebt gekozen voor " + '"' + "Netsky" + '"')
+        console.log("Ze zijn de artist achter de hit: We Can Only Live Today")
+    break;
+
+    case 4:
+        console.log("Je hebt gekozen voor " + '"' + "Mijne blauwe geschelpte" + '"')
+        console.log("Ze zijn de artist achter de hit: ")
+    break;
+
+    default: 
+        console.log("Ongeldige keuze!")
+    break;
+}
