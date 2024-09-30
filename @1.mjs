44cbb50182b8711f307db415cf72bcdee228011f
Kimberley Thill <ThillKimberleyPro@gmail.com>
2024-09-30 10:11:11 +0200

    @1

---
+++ @1.mjs
@@ -0,0 +1,21 @@
+@1
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+
+let var1 = 11;
+let var2 = "11";
+
+if(var1 == var2)
+{
+    console.log("Variable zijn gelijk!")
+}
+else 
+{
+    console.log("Variable zijn niet gelijk!")
+}
+
+/*Uitleg van het taak: 
+"==" kan een string met een nummer vergelijken, 
+maar "===" kan alleen dezelfde type variabelen vergelijken, 
+dus een string met string en een nummer met nummer.*/
